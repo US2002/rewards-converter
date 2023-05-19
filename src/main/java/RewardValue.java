d0c4f03ab2e835f8c4b51b49358d87311f09ca47
Ujjawal Soni <ujjawal.soni1507@gmail.com>
2023-05-19 15:04:28 +0530

    Create RewardValue.java

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,12 @@
+public class RewardValue {
+    public RewardValue(double cashValue) {
+    }
+
+    public String getCashValue() {
+        return null;
+    }
+
+    public String getMilesValue() {
+        return null;
+    }
+}
